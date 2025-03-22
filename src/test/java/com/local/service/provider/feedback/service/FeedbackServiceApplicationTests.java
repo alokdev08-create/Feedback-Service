ee58bd8171c8ae766d53cde9a85165893c57b962
alokdev08-create <alokjavadev08@gmail.com>
2025-03-22 13:52:28 +0530

    Initial commit

---
+++ src/test/java/com/local/service/provider/feedback/service/FeedbackServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.local.service.provider.feedback.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbackServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
